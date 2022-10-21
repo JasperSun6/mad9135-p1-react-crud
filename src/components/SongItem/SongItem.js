9fef0bc170d6692844fb1a13a88daa12474a1db8
sun00120 <sun00120@algonquinlive.com>
2022-10-21 14:05:07 -0400

    change list item button display and place in row

--- src/components/SongItem/SongItem.js
+++ src/components/SongItem/SongItem.js
@@ -13,45 +13,42 @@ const SongItem = ({ song }) => {
       <div style={edit ? { display: "block" } : { display: "none" }}>
         <SongForm setEdit={setEdit} id={song.id} key={song.id} />
       </div>
-
-      <ListGroupItem
-        className="list"
-        key={song.id}
-        style={edit ? { display: "none" } : { display: "block" }}
-      >
-        <div>
-          <p className="songName">
-            {song.name.charAt(0).toUpperCase() + song.name.slice(1)}
-          </p>
-          <div className="sub-detail">
-            <p>
-              Artist:{" "}
-              {song.artist.charAt(0).toUpperCase() + song.artist.slice(1)}
+      <div style={edit ? { display: "none" } : { display: "block" }}>
+        <ListGroupItem className="list" key={song.id}>
+          <div>
+            <p className="songName">
+              {song.name.charAt(0).toUpperCase() + song.name.slice(1)}
             </p>
-            <p>Released Year: {song.released}</p>
+            <div className="sub-detail">
+              <p>
+                Artist:{" "}
+                {song.artist.charAt(0).toUpperCase() + song.artist.slice(1)}
+              </p>
+              <p>Released Year: {song.released}</p>
+            </div>
           </div>
-        </div>
-        <div className="ml-auto list-button">
-          <Button
-            onClick={() => {
-              setEdit(true);
-              return editSong({
-                id: song.id,
-                name: song.name,
-                artist: song.artist,
-                released: song.released,
-              });
-            }}
-            color="warning"
-            className="btn btn-warning mr-1"
-          >
-            Edit
-          </Button>
-          <Button onClick={() => removeSong(song.id)} color="danger">
-            Delete
-          </Button>
-        </div>
-      </ListGroupItem>
+          <div className="ml-auto list-button">
+            <Button
+              onClick={() => {
+                setEdit(true);
+                return editSong({
+                  id: song.id,
+                  name: song.name,
+                  artist: song.artist,
+                  released: song.released,
+                });
+              }}
+              color="warning"
+              className="btn btn-warning mr-1"
+            >
+              Edit
+            </Button>
+            <Button onClick={() => removeSong(song.id)} color="danger">
+              Delete
+            </Button>
+          </div>
+        </ListGroupItem>
+      </div>
     </Container>
   );
 };
